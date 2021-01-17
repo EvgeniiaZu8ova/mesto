0bca439bed913a0d66eeb3abdaff39e3197efae9
Evgeniya Zubova <ev.zu8ova@yandex.ru>
2021-01-17 15:23:01 +0400

    Заменить имеющиеся в HTML карточки на темплейт, внести соответствующие корректировки в функционал в JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,37 @@
 // Задаём переменные
-// Все pop-up
-let popupEdit = document.querySelector('.popup_edition');
-let popupAdd = document.querySelector('.popup_addition');
-let popupPict = document.querySelector('.popup_picture');
+// Окна pop-up
+const popupEdit = document.querySelector('.popup_edition');
+const popupAdd = document.querySelector('.popup_addition');
+const popupPict = document.querySelector('.popup_picture');
 
 // Кнопки открытия и закрытия pop-up
-let openButtonEdit = document.querySelector('.profile__button_edit');
-let openButtonAdd = document.querySelector('.profile__button_add');
-let closeButtonEdit = document.querySelector('.popup__close-icon_edit');
-let closeButtonAdd = document.querySelector('.popup__close-icon_add');
-let closeButtonPict = document.querySelector('.popup__close-icon_pic');
+const openButtonEdit = document.querySelector('.profile__button_edit');
+const openButtonAdd = document.querySelector('.profile__button_add');
+const closeButtonEdit = document.querySelector('.popup__close-icon_edit');
+const closeButtonAdd = document.querySelector('.popup__close-icon_add');
+const closeButtonPict = document.querySelector('.popup__close-icon_pic');
 
-// Кнопки "Нравится" и "Удалить"
-let likeButtons = document.querySelectorAll('.article__button');
-let deleteButtons = document.querySelectorAll('.article__delete');
+// Элементы pop-up окна для просмотра карточек
+const popupImage = document.querySelector('.popup__image');
+const popupParagraph = document.querySelector('.popup__paragraph');
 
 // Элементы форм отправки
-let formElementEdit = document.querySelector('.popup__container_edition');
-let formElementAdd = document.querySelector('.popup__container_addition');
-
-// Переменные для работы с карточками
-let cards = document.querySelectorAll('.article');
-let popupImage = document.querySelector('.popup__image');
-let popupParagraph = document.querySelector('.popup__paragraph');
-
-// Переменные для работы с данными, введенными пользователем
-let nameElement = document.querySelector('.profile__title');
-let jobElement = document.querySelector('.profile__subtitle');
-let placeElement = document.querySelector('.article__name');
-let linkElement = document.querySelector('.article__image');
-
-// Массивы со всеми изображениями и их подписями, имеющимися на странице
-let placeElements = document.querySelectorAll('.article__name');
-let linkElements = document.querySelectorAll('.article__image');
+const formElementEdit = document.querySelector('.popup__container_edition');
+const formElementAdd = document.querySelector('.popup__container_addition');
 
 // Заполняемые пользователем поля
-let nameInput = document.querySelector('.popup__text_input_name');
-let jobInput = document.querySelector('.popup__text_input_job');
-let placeInput = document.querySelector('.popup__text_input_place');
-let linkInput = document.querySelector('.popup__text_input_link');
+const nameInput = document.querySelector('.popup__text_input_name');
+const jobInput = document.querySelector('.popup__text_input_job');
+const placeInput = document.querySelector('.popup__text_input_place');
+const linkInput = document.querySelector('.popup__text_input_link');
+
+// Переменные для работы с данными, введенными пользователем
+const nameElement = document.querySelector('.profile__title');
+const jobElement = document.querySelector('.profile__subtitle');
+
+// Работа с темплейтом
+const template = document.querySelector('template').content;
+const cardsContainer = document.querySelector('.elements');
 
 // Массив с исходными карточками
 const initialCards = [
@@ -68,10 +61,37 @@ const initialCards = [
   }
 ];
 
-// Передаём в форму с карточками названия и изображения из массива initialCards
-for (let i = 0; i < placeElements.length; i++) {
-  placeElements[i].textContent = initialCards[i].name;
-  linkElements[i].src = initialCards[i].link;
+// Добавляем на страницу начальные карточки из массива initialCards
+for (let i = 0; i < initialCards.length; i++) {
+  // Копируем свёрстанный темплейт
+  const cardCopy = template.cloneNode(true);
+  // Заполняем копии данными из массива
+  cardCopy.querySelector('.article__image').src = initialCards[i].link;
+  cardCopy.querySelector('.article__image').alt = initialCards[i].name;
+  cardCopy.querySelector('.article__name').textContent = initialCards[i].name;
+  // Добавляем карточки в DOM
+  cardsContainer.append(cardCopy);
+};
+
+// Инициализируем массивы с элементами имеющихся на странице карточек
+const cards = document.querySelectorAll('.article');
+const linkElements = document.querySelectorAll('.article__image');
+const placeElements = document.querySelectorAll('.article__name');
+const likeButtons = document.querySelectorAll('.article__like-button');
+const deleteButtons = document.querySelectorAll('.article__delete-button');
+
+// Функция добавления новой карточки
+function handleAddFormSubmit (evt) {
+  // Отменяем автоматическое обновление страницы при отправке формы
+  evt.preventDefault();
+  // Копируем темплейт, заполняем копию введёнными пользователем данными
+  const cardCopy = template.cloneNode(true);
+  cardCopy.querySelector('.article__image').src = linkInput.value;
+  cardCopy.querySelector('.article__image').alt = placeInput.value;
+  cardCopy.querySelector('.article__name').textContent = placeInput.value;
+  // Добавляем новую карточку в начало секции
+  cardsContainer.prepend(cardCopy);
+  closePopup(popupAdd);
 }
 
 // Функция открытия формы
@@ -94,11 +114,11 @@ function closePopup(popup) {
 // Функция активации кнопки лайка
 function likePicture (like) {
   // Проверяем, содержит ли кнопка лайка модификатор активной кнопки
-  if (!(like.classList.contains('article__button_active'))) {
+  if (!(like.classList.contains('article__like-button_active'))) {
     // Если не содержит, добавляем; в противном случае - убираем модификатор
-    like.classList.add('article__button_active');
+    like.classList.add('article__like-button_active');
   } else {
-    like.classList.remove('article__button_active');
+    like.classList.remove('article__like-button_active');
   }
 }
 
@@ -117,26 +137,11 @@ function handleEditFormSubmit (evt) {
   closePopup(popupEdit);
 }
 
-// Функция добавления новой карточки и обновления поля с карточками
-function handleAddFormSubmit (evt) {
-  // Отменяем автоматическое обновление страницы при отправке формы
-  evt.preventDefault();
-  // Сдвигаем все карточки на одну вправо, освобождая место для новой карточки
-  const a = placeElements.length;
-  for (let i = 1; i < a; i++) {
-    placeElements[a - i].textContent = placeElements[a - i - 1].textContent;
-    linkElements[a - i].src = linkElements[a - i - 1].src;
-  }
-  // Добавляем в первое поле для карточек свою, новую карточку
-  placeElement.textContent = placeInput.value;
-  linkElement.src = linkInput.value;
-  closePopup(popupAdd);
-}
-
 // Добавляем слушатели
 openButtonEdit.addEventListener('click', function () {
   openPopup(popupEdit);
 });
+
 openButtonAdd.addEventListener('click', function () {
   openPopup(popupAdd);
 });
@@ -144,35 +149,38 @@ openButtonAdd.addEventListener('click', function () {
 closeButtonEdit.addEventListener('click', function () {
   closePopup(popupEdit);
 });
+
 closeButtonAdd.addEventListener('click', function () {
   closePopup(popupAdd);
-})
+});
+
 closeButtonPict.addEventListener('click', function () {
   closePopup(popupPict);
-})
+});
 
-// Необходимо, чтобы можно было применить возможность поставить лайк ко всем элементам с картинками
+formElementEdit.addEventListener('submit', handleEditFormSubmit);
+
+formElementAdd.addEventListener('submit', handleAddFormSubmit);
+
+// Необходимо, чтобы можно было применить возможность поставить лайк ко всем элементам с картинками, поэтому используем цикл
 for (let i = 0; i < likeButtons.length; i++) {
   likeButtons[i].addEventListener('click', function () {
     likePicture(likeButtons[i]);
   })
 }
 
-// Необходимо, чтобы можно было удалять карточки с использованием всех иконок удаления
+// Необходимо, чтобы можно было удалять карточки с использованием всех иконок удаления, поэтому используем цикл
 for (let i = 0; i < deleteButtons.length; i++) {
   deleteButtons[i].addEventListener('click', function () {
     deleteCard(cards[i]);
   })
 }
 
-// Необходимо, чтобы можно было вызвать поп-ап с картинкой при нажатии на любую карточку
+// Необходимо, чтобы можно было вызвать поп-ап с картинкой при нажатии на любую карточку, поэтому используем цикл
 for (let i = 0; i < linkElements.length; i++) {
   linkElements[i].addEventListener('click', function () {
-    popupImage.src = linkElements[i].src;
-    popupParagraph.textContent = placeElements[i].textContent;
-    openPopup(popupPict);
+  popupImage.src = linkElements[i].src;
+  popupParagraph.textContent = placeElements[i].textContent;
+  openPopup(popupPict);
   })
 }
-
-formElementEdit.addEventListener('submit', handleEditFormSubmit);
-formElementAdd.addEventListener('submit', handleAddFormSubmit);
